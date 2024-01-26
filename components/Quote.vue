f614ad0c4b67eb8e067137b8a4f9cb6105bbe9bf
Fazekas Cosmin <fazekascosmin1@gmail.com>
2024-01-26 22:07:01 +0200

    chore: add copy function
    Add a function to copy the quote and author

--- components/Quote.vue
+++ components/Quote.vue
@@ -1,15 +1,38 @@
 <template>
 	<div
-		class="bg-white w-3/4 h-2/5 rounded-2xl flex flex-col justify-between p-2"
+		class="bg-white w-3/4 min-h-96 rounded-2xl flex flex-col justify-between p-2"
 	>
 		<img src="/img/quote-left-solid.svg" alt="quote icon" class="w-16" />
-		<p class="text-2xl italic text-main text-center">
-			{{ quote }}
-		</p>
-		<p class="text-left text-lg text-main self-end m-2">{{ autor }} -</p>
+
+		<!-- QUOTE -->
+		<div class="m-3">
+			<p class="text-2xl italic text-main text-center">
+				{{ quote }}
+			</p>
+			<p class="text-xl text-main text-end mt-2">{{ autor }} -</p>
+		</div>
+
+		<!-- BUTTONS -->
+		<div class="self-end flex m-2">
+			<img
+				src="/img/copy-solid.svg"
+				alt="copy"
+				class="w-12 mx-5 cursor-pointer"
+				@click="copy()"
+			/>
+			<img
+				src="/img/arrows-rotate-solid.svg"
+				alt="refresh"
+				class="w-12 cursor-pointer"
+			/>
+		</div>
 	</div>
 </template>
 
 <script setup>
 	const props = defineProps(["quote", "autor"]);
+
+	function copy() {
+		navigator.clipboard.writeText(`"${props.quote}" - ${props.autor}`);
+	}
 </script>
