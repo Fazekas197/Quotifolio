02f6a6d95b83b47b873f0ff6f65e28ef9fb8f80f
Fazekas Cosmin <fazekascosmin1@gmail.com>
2024-01-29 13:33:14 +0200

    chore: create nav bar

---
+++ components/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<div v-if="isLogged">
+			<button>Sign In</button>
+			<button>Sign Up</button>
+		</div>
+		<button v-else>Log Out</button>
+	</div>
+</template>
+
+<script setup>
+	const isLogged = false;
+</script>
