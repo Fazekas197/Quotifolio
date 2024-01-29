acc3ddeaa31ee1392a5c2b771362f50194c023c4
Fazekas Cosmin <fazekascosmin1@gmail.com>
2024-01-29 12:59:59 +0200

    chore: add register page

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -12,9 +12,7 @@
 				placeholder="Password"
 				class="bg-main opacity-80 w-full text-white text-lg p-2 rounded-xl my-5"
 			/>
-			<button
-				class="bg-main text-white py-2 px-4 rounded-full text-lg float-right"
-			>
+			<button class="bg-main text-white py-2 px-4 rounded-full text-lg">
 				Log In
 			</button>
 		</form>
--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,3 +1,30 @@
 <template>
-	<div></div>
+	<div class="bg-white w-3/4 min-h-96 rounded-2xl">
+		<h1 class="text-center text-4xl text-main my-5">Register</h1>
+		<form @submit.prevent="" class="m-5 floa">
+			<input
+				type="email"
+				placeholder="Email"
+				class="bg-main opacity-80 w-full text-white text-lg p-2 rounded-xl"
+			/>
+			<input
+				type="text"
+				placeholder="Username"
+				class="bg-main opacity-80 w-full text-white text-lg p-2 rounded-xl mt-3"
+			/>
+			<input
+				type="password"
+				placeholder="Password"
+				class="bg-main opacity-80 w-full text-white text-lg p-2 rounded-xl mt-3"
+			/>
+			<input
+				type="password"
+				placeholder="Confirm Password"
+				class="bg-main opacity-80 w-full text-white text-lg p-2 rounded-xl my-3"
+			/>
+			<button class="bg-main text-white py-2 px-4 rounded-full text-lg">
+				Confirm
+			</button>
+		</form>
+	</div>
 </template>
