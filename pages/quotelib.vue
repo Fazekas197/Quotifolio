013811dc12c5dbb7d13be1053fbedc284fbc6e9d
Fazekas Cosmin <fazekascosmin1@gmail.com>
2024-01-31 16:23:02 +0200

    fix: when there are no quotes a msg will appear

--- pages/quotelib.vue
+++ pages/quotelib.vue
@@ -1,23 +1,33 @@
 <template>
 	<div class="m-auto">
-		<Quote
-			:quote="quotes[index].quote"
-			:autor="quotes[index].author"
-			class="m-auto"
-		/>
-		<div class="text-center">
-			<button
-				class="bg-main text-white py-2 px-4 rounded-full"
-				@click="prev"
+		<div v-if="quotes.length >= 1">
+			<Quote
+				:quote="quotes[index].quote"
+				:autor="quotes[index].author"
+				class="m-auto"
+			/>
+			<div class="text-center">
+				<button
+					class="bg-main text-white py-2 px-4 rounded-full"
+					@click="prev"
+				>
+					previous
+				</button>
+				<button
+					class="bg-main text-white py-2 px-4 rounded-full mx-4"
+					@click="next"
+				>
+					next
+				</button>
+			</div>
+		</div>
+		<div v-else class="text-2xl text-main text-center">
+			<p>There are no quotes</p>
+			<NuxtLink
+				to="/addquote"
+				class="bg-main text-white py-2 rounded-full block m-2"
+				>Add one?</NuxtLink
 			>
-				previous
-			</button>
-			<button
-				class="bg-main text-white py-2 px-4 rounded-full mx-4"
-				@click="next"
-			>
-				next
-			</button>
 		</div>
 	</div>
 </template>
