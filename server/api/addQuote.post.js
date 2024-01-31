b5129a5be7c6958da6e62d7f42ac11099d2b1748
Fazekas Cosmin <fazekascosmin1@gmail.com>
2024-01-31 02:39:13 +0200

    chore: api to post new quotes

--- server/api/addQuote.post.js
+++ server/api/addQuote.post.js
@@ -2,4 +2,8 @@ import { User } from "../models/User";
 
 export default defineEventHandler(async (event) => {
 	const body = await readBody(event);
+	await User.updateOne(
+		{ _id: body.id },
+		{ $push: { quotes: { quote: body.quote, author: body.author } } }
+	);
 });
