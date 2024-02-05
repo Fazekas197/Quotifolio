089ae5a29d5729b36c18a50a4fecd831f7d3d401
Fazekas Cosmin <fazekascosmin1@gmail.com>
2024-02-05 23:46:36 +0200

    refactor: testing if api works in prod

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,20 @@
 <template>
-	<h1 class="text-center text-4xl text-main my-5">Welcome</h1>
+	<Quote :quote="quote.q" :autor="quote.a" class="m-auto">
+		<img
+			src="/img/arrows-rotate-solid.svg"
+			alt="refresh"
+			class="w-12 cursor-pointer"
+			@click="getQuote()"
+		/>
+	</Quote>
 </template>
+
+<script setup>
+	const { data } = await useFetch("https://zenquotes.io/api/quotes/");
+	const quote = ref(data._rawValue[0]);
+
+	function getQuote() {
+		const index = Math.floor(Math.random() * 50);
+		quote.value = data._rawValue[index];
+	}
+</script>
